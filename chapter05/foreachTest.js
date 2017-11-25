de2e180cec4c18bbb2ad44bbcf0488d79387fe42
Rahisul Haque <rahisulhaque@Rahisuls-MacBook-Air.local>
2017-11-24 17:14:44 -0800

    adding new stuff

---
+++ chapter05/foreachTest.js
@@ -0,0 +1,13 @@
+const arr = ['a', 'b', 'c'];
+
+arr.forEach(function(element) {
+    console.log(element);
+});
+
+
+
+var myArray = ["Jeff Bezo", "Bill Gates", "Elon Mask"];
+myArray.forEach(function (element){
+	console.log(" I want to be " + element + " !!!")
+});
+
